8ce2ea80b3d8ab5b999162207580c3be5c1bed58
Digvijay Suryawanshi <suryawanshidigvijay@gmail.com>
2019-08-27 12:37:23 +0530

    Initial code upload

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    /**
+     * DashboardController constructor.
+     */
+    public function __construct()
+    {
+        // To Do
+    }
+
+    public function index()
+    {
+
+    }
+}
